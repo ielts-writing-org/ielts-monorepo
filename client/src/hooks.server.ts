72c2a9e5d54db5128bc521bfc5ec047a9e333b1e
Phùng Văn Tú <tuphung.03032004@gmail.com>
2026-09-14 20:23:55 +0700

    fix(auth): handle session retrieval errors in hooks.server.ts

--- client/src/hooks.server.ts
+++ client/src/hooks.server.ts
@@ -2,15 +2,19 @@ import { authClient } from "$lib/shared/auth-client";
 import { type Handle } from "@sveltejs/kit";
 
 export const handle: Handle = async ({ event, resolve }) => {
-	const session = await authClient.getSession({
-		fetchOptions: {
-			headers: event.request.headers
-		}
-	});
+	try {
+		const session = await authClient.getSession({
+			fetchOptions: {
+				headers: event.request.headers
+			}
+		});
 
-	if (session) {
-		event.locals.session = session.data?.session;
-		event.locals.user = session.data?.user;
+		if (session) {
+			event.locals.session = session.data?.session;
+			event.locals.user = session.data?.user;
+		}
+	} catch {
+		throw new Error("Unable to contact remote server");
 	}
 
 	const theme = event.cookies.get("theme");
